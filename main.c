19b20606761ab46a5281b5aa5b3ad0299f7f9032
jllgame00 <bunaro4501@naver.com>
2019-10-23 21:34:13 +0900

    평균 힘 추가

--- main.c
+++ main.c
@@ -10,8 +10,9 @@ int main()
     int pp;
     int if1, it;
     int ii;
+    int ft, fmv;
 
-    printf("힘을 구하고 싶으면 1을, 운동량을 구하고 싶으면 2를, 충격량을 구하고 싶으면 3을 입력하세요: ");
+    printf("힘을 구하고 싶으면 1을, 운동량을 구하고 싶으면 2를, 충격량을 구하고 싶으면 3을, 평균 힘을 구하고 싶다면 4를 입력하세요: ");
     scanf("%d", &num1);
 
     if (num1 == 1)
@@ -34,7 +35,7 @@ int main()
         printf("구하고 싶은 운동량은 %d 입니다.", pp);
         }
 
-    else
+    else if (num1 == 3)
         {
         printf("구하고 싶은 충격량에 대한 질량과 속도를 각각 입력하세요: \n");
         scanf("%d %d", &if1, &it);
@@ -42,7 +43,16 @@ int main()
         ii = if1 * it;
 
         printf("구하고 싶은 충격량은 %d 입니다.", ii);
+        }
 
+    else
+        {
+        printf("구하고 싶은 평균 힘에 대한 충돌량과 충돌 시간이나과 운동량의 변화량과 시간을를 각각 입력하세요: \n");
+        scanf("%d %d", &fmv, &ft);
+
+        ff = fmv / ft;
+
+        printf("구하고 싶은 충격량은 %d 입니다.", ff);
         }
     return 0;
 }
